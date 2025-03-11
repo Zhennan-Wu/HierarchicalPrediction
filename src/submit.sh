defa44cd9445963a379dd46ed32fc4d2f18cd2b8
Leo Wu ROG <zwu1@iu.edu>
2025-03-10 22:50:18 -0400

    hpc

--- src/submit.sh
+++ src/submit.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-#SBATCH -J dbn_gm_new3
-#SBATCH -p gpu
+#SBATCH -J dbn_ccl
+#SBATCH -p hopper
 #SBATCH -o ../outputs/output_%j.txt
 #SBATCH -e ../outputs/error_%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=zwu1@iu.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=20:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --time=2:00:00:00
 #SBATCH --mem=32G
 #SBATCH -A r00939
+#SBATCH --gpus-per-node=1
 
 #Load any modules that your program needs
 module load conda/24.1.2
@@ -20,7 +21,7 @@ module load nvidia/21.5
 conda activate hdp
 
 #Run your program
-srun python3 ./dbn.py 
+srun python3 ./train_dbn.py 
 # srun python3 ./dbm.py
 # srun python3 ./hdp.py
 # srun python3 ./hdp_dbm.py
